6fae65676e6d9a5158e68a23b73ffe8aab72a289
ReflextionsDev <ReflextionsDev@gmail.com>
2021-10-13 12:54:57 -0400

    Add core structure

---
+++ main.js
@@ -0,0 +1,92 @@
+// REQs
+// The user is able to 'catch' fish with randomly generated names, weights, and values
+// The time of day is shown to the user
+// The user can only fish for six hours
+// The user can only catch at maximum 10 lbs of fish
+// Each turn, the sum of the user's caught fishes' weight and value is displayed
+// You make at least three commits in git (after completing parts of the project)
+// At the end of the game, all the fish that the user caught are displayed along with a sum of their weight and value
+
+// Prompt sync
+const prompt = require('prompt-sync')({ sigint: true })
+
+// Game vars
+const timeStart = 6
+const timeLimit = 6
+const timeMax = timeStart + timeLimit
+const weightMax = 10
+
+// Player vars
+let weight = 0
+let timePassed = 0
+let fishCaught = 0
+let money = 0
+
+let yeet = "gadsfgsf"
+
+// Fish objects > name, weight, value
+
+// Game
+navMain()
+navBreak()
+navStatus()
+
+
+function navMain() {
+    console.log("You've gone fishing! Try to maximize the value of your caught fish. You can fish for " + timeLimit + " hours (till " + formatTime(timeMax) + ") and can catch at most " + weightMax + " lbs of fish.")
+    navBreak()
+}
+
+function navBreak() {
+    console.log('\n' + '=====================================' + '\n')
+}
+
+function navStatus() {
+    console.log("The time is " + formatTime(timeStart + timePassed) + ". So far you've caught: ")
+    console.log(fishCaught + " fish, " + weight + " lbs, " + "$0.00")
+
+    console.log("Press any button to continue")
+    prompt('')
+
+    navFish()
+}
+
+
+
+// replace with fish obj
+
+function navFish() {
+    console.log("You caught a " + yeet + " weighing " + yeet + " lbs and valued at " + yeet)
+
+    console.log("Your action: [c]atch or [r]elease?")
+    let input = prompt("")
+
+    while (input !== 'c' && input !== 'r') {
+        console.log("Please enter [c] or [r]")
+        input = prompt("")
+    }
+
+    if (input === 'c') {
+        console.log("catch")
+    } else if (input === 'r') {
+        console.log("release")
+    }
+}
+
+
+// Returns a string in 12hr format
+function formatTime(time) {
+
+    let m = (time % 24) < 12 ? 'am' : 'pm'
+    let hours = (Math.floor(time + 11) % 12) + 1
+    let minutes = Math.round(60 * (time % 1))
+    minutes = minutes.toString().padStart(2, '0')
+
+    let timeString = hours + ':' + minutes + m
+    return timeString
+}
+
+// To Do
+function formatMoney(money) {
+    return money
+}
