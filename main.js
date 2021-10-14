d4b0a2e046f19b61678013a8668d2b60bb321b5a
ReflextionsDev <ReflextionsDev@gmail.com>
2021-10-14 11:19:09 -0400

    Add weight limit

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-// Stretch goals > make weight determined by desc 1, and value mult from desc2
-
-// REQs
-// The user can only catch at maximum 10 lbs of fish
-// Each turn, the sum of the user's caught fishes' weight and value is displayed
-// At the end of the game, all the fish that the user caught are displayed along with a sum of their weight and value
+// Stretch goals
+// Make weight mult determined by desc 1, and value mult from desc2
 
 // Prompt sync
 const prompt = require('prompt-sync')({ sigint: true })
@@ -47,16 +43,13 @@ function navBreak() {
 
 function navMain() {
 
-    // Delete me
-    console.log(fish)
-
     updateStats()
 
     if (timePassed >= timeLimit) {
         navEnd()
     } else {
         console.log("The time is " + formatTime(timeStart + timePassed) + ". So far you've caught: ")
-        console.log(fishCaught + " fish, " + totalWeight + " lbs, " + formatMoney(totalValue))
+        console.log(fishCaught + " fish, " + formatWeight(totalWeight) + ", " + formatMoney(totalValue))
         console.log("Press any button to continue")
         prompt('')
         navFish()
@@ -66,22 +59,27 @@ function navMain() {
 
 function navFish() {
 
+    navBreak()
     let tempFish = getFish()
+    console.log("You caught a " + tempFish.name + " weighing " + formatWeight(tempFish.weight) + ", and valued at " + formatMoney(tempFish.value))
 
-    console.log("You caught a " + tempFish.name + " weighing " + tempFish.weight + "lbs and valued at " + formatMoney(tempFish.value))
-    console.log("Your action: [c]atch or [r]elease?")
-    let input = prompt("")
+    if ((Number(totalWeight) + Number(tempFish.weight)) > weightLimit) {
+        console.log("This fish would put you over " + weightLimit + "lbs, so you release it.")
+    } else {
+        console.log("Your action: [c]atch or [r]elease?")
+        let input = prompt("")
 
-    while (input !== 'c' && input !== 'r') {
-        console.log("Please enter [c] or [r]")
-        input = prompt("")
-    }
+        while (input !== 'c' && input !== 'r') {
+            console.log("Please enter [c] or [r]")
+            input = prompt("")
+        }
 
-    if (input === 'c') {
-        console.log("You chose to keep the fish.")
-        fish.push(tempFish)
-    } else if (input === 'r') {
-        console.log("You chose to release the fish.")
+        if (input === 'c') {
+            console.log("You chose to keep the fish.")
+            fish.push(tempFish)
+        } else if (input === 'r') {
+            console.log("You chose to release the fish.")
+        }
     }
 
     timePassed++
@@ -91,17 +89,17 @@ function navFish() {
 
 
 function navEnd() {
-    console.log("The time is " + formatTime(timeStart + timePassed) + ". Time's up!")
+    console.log("The time is " + formatTime(timeStart + timePassed) + ". Time's up!" + '\n')
     console.log("You caught " + fishCaught + " fish: ")
-    // To Do
+    console.log()
 
     for (let i = 0; i < fish.length; i++) {
-   
-    
-        console.log('* ' + fish[i].name + ", " + fish[i].weight + "lbs, " + formatMoney(fish[i].value))
-        
+        console.log('* ' + fish[i].name + ", " + formatWeight(fish[i].weight) + ", " + formatMoney(fish[i].value))
     }
 
+    console.log()
+    console.log(`Total weight: ${totalWeight}lbs`)
+    console.log(`Total value: ${formatMoney(totalValue)}`)
 }
 
 
@@ -122,6 +120,12 @@ function formatTime(time) {
 // Formats money into dollar format ($1.03)
 function formatMoney(money) {
     let output = '$' + Number(money).toFixed(2)
+    return output
+}
+
+// Formats weight (12.32lbs)
+function formatWeight(weight) {
+    let output = Number(weight).toFixed(2) + 'lbs'
     return output
 }
 
@@ -166,7 +170,7 @@ function updateStats() {
     for (let i = 0; i < fish.length; i++) {
         const index = fish[i]
         fishCaught++
-        totalWeight+= Number(fish[i].weight)
-        totalValue+= Number(fish[i].value)     
+        totalWeight += Number(fish[i].weight)
+        totalValue += Number(fish[i].value)
     }
 }
